4d5ff3cf1a443e30cbff5aa1fdaba82ca62df7d2
joao.carrion <joao.carrion@gmail.com>
2024-09-14 10:26:19 -0300

    new rules for reviewed database

--- functions/index.js
+++ functions/index.js
@@ -7,28 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
+// INITIALIZE
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const logger = require("firebase-functions/logger");
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
-logger.log('Hello, logs!');
-exports.updateCategoryMetadata = functions.firestore
-	.document('group/{groupId}/categories/{categoryId}')
-	.onWrite((change, context) => {
-		logger.log('updateCategoryMetadata');
-		const groupId = context.params.groupId;
-		const categoryId = context.params.categoryId;
-		logger.log(`Category ${categoryId} updated in group ${groupId}`);
-
-		// Get the metadata document reference
-		const metadataRef = admin.firestore().collection('groups').doc(groupId).collection('metadata').doc('categories');
-
-		// Update the metadata document with the server timestamp
-		return metadataRef.set({
-			lastUpdated: admin.firestore.FieldValue.serverTimestamp()
-		}, { merge: true });
-	});
+// const functions = require('firebase-functions');
+// const firestore = admin.firestore();
+// const { Firestore } = require('firebase-admin/firestore')
