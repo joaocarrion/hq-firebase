0f3bf8a051f2ee2a92a60c7dc328ecd620a5079c
joao.carrion <joao.carrion@gmail.com>
2025-08-05 14:56:52 -0300

    exporting cleanup

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,4 +1,5 @@
 // INITIALIZE
-export { acceptInvitation, rejectInvitation } from './model/invitation.js';
-export { removeUser, leaveGroup, eraseUser } from './model/user.js';
-export { createInvitation } from './model/group.js';
+export { acceptInvitation, rejectInvitation } from "./model/invitation.js";
+export { removeUser, leaveGroup, eraseUser } from "./model/user.js";
+export { createInvitation } from "./model/group.js";
+export { cleanExpired } from "./model/clean.js";
