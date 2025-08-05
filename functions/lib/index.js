e54d6300bfb00c734363526a83aa8b9e9bb9ba06
joao.carrion <joao.carrion@gmail.com>
2025-08-05 15:02:38 -0300

    removing lib

--- functions/lib/index.js
+++
@@ -1,5 +0,0 @@
-// INITIALIZE
-export { acceptInvitation, rejectInvitation } from "./model/invitation.js";
-export { removeUser, leaveGroup, eraseUser } from "./model/user.js";
-export { createInvitation } from "./model/group.js";
-export { cleanExpired } from "./model/clean.js";
